471b38e338b502dc1466ddf3da80888763f37794
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2024-09-04 02:28:39 -0300

    Part one exercises anecdotes finish

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,16 +11,36 @@ const App = () => {
         "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
         "The only way to go fast, is to go well."
     ];
-
+    const [points, setPoints] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
     const [selected, setSelected] = useState(0);
+    const copy = [...points];
 
     const getRandomInt = (max) => Math.floor(Math.random() * max);
     const handleClick = () => setSelected(getRandomInt(anecdotes.length));
+    const handleVote = () => {
+        copy[selected] += 1;
+        setPoints(copy);
+    };
+
+    let max = copy[0];
+    let maxPosition = 0;
+
+    for (let i = 1; i < copy.length; i++) {
+        if (copy[i] > max) {
+            max = copy[i];
+            maxPosition = i;
+        }
+    }
 
     return (
         <>
-            <div>{anecdotes[selected]}</div>
+            <h1>Anecdote of the day</h1>
+            <div>{anecdotes[selected]} </div>
+            <div>has {points[selected]} votes</div>
+            <button onClick={handleVote}>Vote</button>
             <button onClick={handleClick}>next anecdote</button>
+            <h1>Anecdote with most votes</h1>
+            <div>{anecdotes[maxPosition]}</div>
         </>
     );
 };
